c6a4b6f249e613b383ce9515ac58273b3128ac51
AlejoGomez1702 <luis.gomezc@autonoma.edu.co>
2020-02-12 10:33:15 -0500

    Scaffolding authentication module

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User();
+        $admin->name = "administrador";
+        $admin->email = "admin@mail.com";
+        $admin->password = bcrypt('admin');
+        $admin->save();
+    }
+}
